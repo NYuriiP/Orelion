19017016314c52727c463cc4100a4771ca863e47
Nathan Yurii Pycio <87712593+NYuriiP@users.noreply.github.com>
2025-01-29 12:14:31 +0000

    Update textFormatting.py

---
+++ textFormatting.py
@@ -0,0 +1,30 @@
+olors = {
+  "red": "\033[31m",
+  "green": "\033[32m",
+  "yellow": "\033[33m",
+  "blue": "\033[34m",
+  "magenta": "\033[35m",
+  "cyan": "\033[36m",
+  "purple": "\033[35m",
+  "white": "\033[37m",
+  "gray": "\033[90m",
+  "light_red": "\033[91m",
+  "light_green": "\033[92m",
+  "light_yellow": "\033[93m",
+  "light_blue": "\033[94m",
+  "light_magenta": "\033[95m",
+  "light_cyan": "\033[96m",
+  "pale_blue": "\033[94m",
+  "dark_blue": "\033[34m",
+  "reset": "\033[0m"
+}
+
+format = {
+"bold": "\033[1m",
+"dim": "\033[2m",
+"underline": "\033[4m",
+"blink": "\033[5m",
+"reverse": "\033[7m",
+"hidden": "\033[8m",
+"reset": "\033[0m"
+}
